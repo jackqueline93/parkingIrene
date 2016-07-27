df95cf5dc9ec9f763e246cd157e4e273b5c3eb8f
Karen Diaz <karen_dt@outlook.com>
2016-07-27 22:30:29 +0000

    Todas las entidades creadas con vistas

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+
+  before_filter :authenticate_user!
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+end
